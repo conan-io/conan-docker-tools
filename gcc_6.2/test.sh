696a4dd784369f007aece16f8b166667be1d5031
Luis Martinez de Bartolome <lasote@gmail.com>
2017-05-30 16:27:04 +0200

    update cmake in progress

--- gcc_6.2/test.sh
+++ gcc_6.2/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --rm lasote/conangcc62 /bin/bash -c "conan user && conan install gtest/1.8.0@lasote/stable -s arch=x86_64 --build && conan install gtest/1.8.0@lasote/stable -s arch=x86 --build"
+sudo docker run --rm lasote/conangcc62 /bin/bash -c "conan user && conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=6.2 --build && conan install gtest/1.8.0@lasote/stable -s arch=x86 --build"
--- gcc_6.3/test.sh
+++ gcc_6.3/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --rm lasote/conangcc63 /bin/bash -c "conan user && conan install gtest/1.8.0@lasote/stable -s arch=x86_64 --build && conan install gtest/1.8.0@lasote/stable -s arch=x86 --build"
+sudo docker run --rm lasote/conangcc63 /bin/bash -c "conan user && conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=6.3 --build && conan install gtest/1.8.0@lasote/stable -s arch=x86 --build"
