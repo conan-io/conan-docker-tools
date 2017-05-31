7a42d877ac424283db1b99dc39f08ab1dd03acfc
Uilian Ries <uilianries@gmail.com>
2017-05-31 19:36:37 -0300

    Split test by command
    - FIX: Break unique test line to prevent hidden error
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- gcc_5.2/test.sh
+++ gcc_5.2/test.sh
@@ -1,1 +1,8 @@
-sudo docker run --rm lasote/conangcc52 /bin/sh -c "sudo pip install conan-package_tools --upgrade && sudo pip install conan --upgrade && conan user && conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=5.2 --build && conan install gtest/1.8.0@lasote/stable -s arch=x86 -s compiler=gcc -s compiler.version=5.2 --build"
+sudo docker run -t -d --name conangcc49 lasote/conangcc49 && \
+sudo docker exec conangcc49 sudo pip install -U conan_package_tools && \
+sudo docker exec conangcc49 sudo pip install -U conan && \
+sudo docker exec conangcc49 conan user && \
+sudo docker exec conangcc49 conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=5.2 -s compiler.libcxx=libstdc++ --build && \
+sudo docker exec conangcc49 conan install gtest/1.8.0@lasote/stable -s arch=x86 -s compiler=gcc -s compiler.version=5.2 -s compiler.libcxx=libstdc++ --build && \
+sudo docker stop conangcc49 && \
+sudo docker rm conangcc49
--- gcc_6.2/test.sh
+++ gcc_6.2/test.sh
@@ -1,1 +1,8 @@
-sudo docker run --rm lasote/conangcc62 /bin/sh -c "sudo pip install conan-package_tools --upgrade && sudo pip install conan --upgrade && conan user && conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=6.2 --build && conan install gtest/1.8.0@lasote/stable -s arch=x86 -s compiler=gcc -s compiler.version=6.2 --build"
+sudo docker run -t -d --name conangcc62 lasote/conangcc62 && \
+sudo docker exec conangcc62 sudo pip install -U conan_package_tools && \
+sudo docker exec conangcc62 sudo pip install -U conan && \
+sudo docker exec conangcc62 conan user && \
+sudo docker exec conangcc62 conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=6.2 -s compiler.libcxx=libstdc++ --build && \
+sudo docker exec conangcc62 conan install gtest/1.8.0@lasote/stable -s arch=x86 -s compiler=gcc -s compiler.version=6.2 -s compiler.libcxx=libstdc++ --build && \
+sudo docker stop conangcc62 && \
+sudo docker rm conangcc62
