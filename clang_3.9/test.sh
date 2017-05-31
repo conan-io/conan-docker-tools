d5fa23dfad4dcf92d7aa70733e54b71c6c2ecc2e
Uilian Ries <uilianries@gmail.com>
2017-05-31 19:52:20 -0300

    Add libc++ support for clang images
    - Added libc++ for x86 and x86_64
    - Added new tests using clang + libc++
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- clang_3.9/test.sh
+++ clang_3.9/test.sh
@@ -4,5 +4,7 @@ sudo docker exec conanclang39 sudo pip install -U conan && \
 sudo docker exec conanclang39 conan user && \
 sudo docker exec conanclang39 conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=clang -s compiler.version=3.9 -s compiler.libcxx=libstdc++ --build && \
 sudo docker exec conanclang39 conan install gtest/1.8.0@lasote/stable -s arch=x86 -s compiler=clang -s compiler.version=3.9 -s compiler.libcxx=libstdc++ --build && \
+sudo docker exec conanclang39 conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=clang -s compiler.version=3.9 -s compiler.libcxx=libc++ --build && \
+sudo docker exec conanclang39 conan install gtest/1.8.0@lasote/stable -s arch=x86 -s compiler=clang -s compiler.version=3.9 -s compiler.libcxx=libc++ --build && \
 sudo docker stop conanclang39 && \
 sudo docker rm conanclang39
