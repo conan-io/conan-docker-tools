a6d590c1abf64c39696e5156f1c4a6b148af007e
Luis Martinez de Bartolome <lasote@gmail.com>
2017-05-31 10:19:33 +0200

    Fixed images with ffi and ssl dev

--- gcc_5.3/test.sh
+++ gcc_5.3/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --rm lasote/conangcc53 /bin/bash -c "conan user && conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=5.3  --build && conan install gtest/1.8.0@lasote/stable -s arch=x86 --build"
+sudo docker run --rm lasote/conangcc53 /bin/bash -c "sudo pip install conan-package_tools --upgrade && sudo pip install conan --upgrade && conan user && conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=5.3  --build && conan install gtest/1.8.0@lasote/stable -s arch=x86 --build"
--- gcc_6.2/test.sh
+++ gcc_6.2/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --rm lasote/conangcc62 /bin/bash -c "conan user && conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=6.2 --build && conan install gtest/1.8.0@lasote/stable -s arch=x86 --build"
+sudo docker run --rm lasote/conangcc62 /bin/bash -c "sudo pip install conan-package_tools --upgrade && sudo pip install conan --upgrade && conan user && conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=6.2 --build && conan install gtest/1.8.0@lasote/stable -s arch=x86 --build"
