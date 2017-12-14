4fe9b37cd59ff292746082d4ccaef90541fef2ae
Luis Martinez de Bartolome <lasote@gmail.com>
2017-12-14 17:19:45 +0100

    major gcc versions

---
+++ gcc_6/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo docker run -t -d --name conangcc6 lasote/conangcc6 && \
+sudo docker exec conangcc6 sudo pip install -U conan_package_tools && \
+sudo docker exec conangcc6 sudo pip install -U conan && \
+sudo docker exec conangcc6 conan user && \
+sudo docker exec conangcc6 conan install gtest/1.8.0@lasote/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=6 -s compiler.libcxx=libstdc++ --build && \
+sudo docker exec conangcc6 conan install gtest/1.8.0@lasote/stable -s arch=x86 -s compiler=gcc -s compiler.version=6 -s compiler.libcxx=libstdc++ --build && \
+sudo docker stop conangcc6 && \
+sudo docker rm conangcc6
