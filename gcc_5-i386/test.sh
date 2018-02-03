b0ad5521c9e36aced9e8a4fa41ddf015c43d3169
Uilian Ries <uilianries@gmail.com>
2018-02-03 20:54:04 -0200

    #31 Add Docker image GCC 5 i386
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ gcc_5-i386/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo docker run -t -d --name conangcc5-i386 lasote/conangcc5-i386 && \
+sudo docker exec conangcc5-i386 sudo pip install -U conan_package_tools && \
+sudo docker exec conangcc5-i386 sudo pip install -U conan && \
+sudo docker exec conangcc5-i386 conan user && \
+sudo docker exec conangcc5-i386 conan install gtest/1.8.0@conan/stable -s arch=x86 -s compiler=gcc -s compiler.version=5 -s compiler.libcxx=libstdc++ --build && \
+sudo docker stop conangcc5-i386 && \
+sudo docker rm conangcc5-i386
