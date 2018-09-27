8665db236267e2256c1b21c1bac64b2543b123ce
SSE4 <tomskside@gmail.com>
2018-09-27 16:06:46 +0700

    - add clang 7.0
    Signed-off-by: SSE4 <tomskside@gmail.com>

---
+++ clang_7.0-x86/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo docker run --rm -v ~/.conan/data:/home/conan/.conan/data -it lasote/conanclang70-x86 /bin/bash
---
+++ clang_7.0-x86/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo docker run -t -d --name conanclang70-x86 lasote/conanclang70-x86 && \
+sudo docker exec conanclang70-x86 sudo pip install -U conan_package_tools && \
+sudo docker exec conanclang70-x86 sudo pip install -U conan && \
+sudo docker exec conanclang70-x86 conan user && \
+sudo docker exec conanclang70-x86 conan install gtest/1.8.0@bincrafters/stable -s arch=x86 -s compiler=clang -s compiler.version=7.0 -s compiler.libcxx=libstdc++ --build && \
+sudo docker exec conanclang70-x86 conan install gtest/1.8.0@bincrafters/stable -s arch=x86 -s compiler=clang -s compiler.version=7.0 -s compiler.libcxx=libc++ --build && \
+sudo docker stop conanclang70-x86 && \
+sudo docker rm conanclang70-x86
