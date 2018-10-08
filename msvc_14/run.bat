74eb8f0a708093ac66ed68ee2892a73b785025cf
Uilian Ries <uilianries@gmail.com>
2018-10-08 13:18:40 -0300

    #50 Update MSVC14
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- msvc_14/run.bat
+++ msvc_14/run.bat
@@ -1,1 +1,1 @@
-docker run --rm -v %USERPROFILE%/.conan/data:C:/Users/ContainerAdministrator/.conan/data -it lasote/conanmsvc14
+docker run --rm -v %USERPROFILE%/.conan/data:"C:/Users/ContainerAdministrator/.conan/data" -it lasote/conanmsvc14
