c515ca8667f4f0b229f8eecbcd1d292990fca11f
Eric Hargitt <eric.hargitt@gmail.com>
2018-10-10 14:37:37 -0400

    Add dpkg statement to add corresponding architecture for gcc49-armv7hf. Add statement to test.sh to pull foreign arch package.

--- gcc_4.9-armv7hf/test.sh
+++ gcc_4.9-armv7hf/test.sh
@@ -4,5 +4,6 @@ sudo docker exec gcc49-armv7hf sudo pip install -U conan_package_tools && \
 sudo docker exec gcc49-armv7hf sudo pip install -U conan && \
 sudo docker exec gcc49-armv7hf conan user && \
 sudo docker exec gcc49-armv7hf conan install gtest/1.8.0@bincrafters/stable -s arch=armv7hf -s compiler=gcc -s compiler.version=4.9 -s compiler.libcxx=libstdc++ --build && \
+sudo docker exec gcc49-armv7hf sudo apt-get install libc6:armhf && \
 sudo docker stop gcc49-armv7hf && \
 sudo docker rm gcc49-armv7hf
