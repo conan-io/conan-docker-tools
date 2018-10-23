e234deb6222b1824100ded2ca1e5a7e294d4cb85
Uilian Ries <uilianries@gmail.com>
2018-10-23 16:17:15 -0300

    #53 Intall python on GCC 6.2
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- gcc_6.2/test.sh
+++ gcc_6.2/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo docker run -t -d --name gcc62 conanio/gcc62 && \
-sudo docker exec gcc62 sudo pip install -U conan_package_tools && \
-sudo docker exec gcc62 sudo pip install -U conan && \
+sudo docker exec gcc62 pip install -U conan_package_tools && \
+sudo docker exec gcc62 pip install -U conan && \
 sudo docker exec gcc62 conan user && \
 sudo docker exec gcc62 conan install gtest/1.8.0@bincrafters/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=6.2 -s compiler.libcxx=libstdc++ --build && \
 sudo docker exec gcc62 conan install gtest/1.8.0@bincrafters/stable -s arch=x86 -s compiler=gcc -s compiler.version=6.2 -s compiler.libcxx=libstdc++ --build && \
