4612d009cd2662679bf417f76c0eb6672ceef5de
Uilian Ries <uilianries@gmail.com>
2018-10-23 17:40:55 -0300

    #53 Install python on Clang 3.8
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- clang_3.8/test.sh
+++ clang_3.8/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo docker run -t -d --name clang38 conanio/clang38 && \
-sudo docker exec clang38 sudo pip install -U conan_package_tools && \
-sudo docker exec clang38 sudo pip install -U conan && \
+sudo docker exec clang38 pip install -U conan_package_tools && \
+sudo docker exec clang38 pip install -U conan && \
 sudo docker exec clang38 conan user && \
 sudo docker exec clang38 conan install gtest/1.8.0@bincrafters/stable -s arch=x86_64 -s compiler=clang -s compiler.version=3.8 -s compiler.libcxx=libstdc++ --build && \
 sudo docker exec clang38 conan install gtest/1.8.0@bincrafters/stable -s arch=x86 -s compiler=clang -s compiler.version=3.8 -s compiler.libcxx=libstdc++ --build && \
