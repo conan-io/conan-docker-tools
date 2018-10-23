dd287904e29ae2ebf3a14cb8b7d21d6cbc21490c
Uilian Ries <uilianries@gmail.com>
2018-10-23 16:12:55 -0300

    #53 Intall python on GCC 6.4
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- gcc_6.4/test.sh
+++ gcc_6.4/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo docker run -t -d --name gcc64 conanio/gcc64 && \
-sudo docker exec gcc64 sudo pip install -U conan_package_tools && \
-sudo docker exec gcc64 sudo pip install -U conan && \
+sudo docker exec gcc64 pip install -U conan_package_tools && \
+sudo docker exec gcc64 pip install -U conan && \
 sudo docker exec gcc64 conan user && \
 sudo docker exec gcc64 conan install gtest/1.8.0@bincrafters/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=6.4 -s compiler.libcxx=libstdc++ --build && \
 sudo docker exec gcc64 conan install gtest/1.8.0@bincrafters/stable -s arch=x86 -s compiler=gcc -s compiler.version=6.4 -s compiler.libcxx=libstdc++ --build && \
