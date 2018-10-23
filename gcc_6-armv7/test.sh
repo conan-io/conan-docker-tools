b0dc2386daacbe66548f616b5313232e0a26f08f
Uilian Ries <uilianries@gmail.com>
2018-10-23 16:23:13 -0300

    #53 Intall python on GCC 6 ARMv7
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- gcc_6-armv7/test.sh
+++ gcc_6-armv7/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo docker run -t -d --name gcc6-armv7 conanio/gcc6-armv7 && \
-sudo docker exec gcc6-armv7 sudo pip install -U conan_package_tools && \
-sudo docker exec gcc6-armv7 sudo pip install -U conan && \
+sudo docker exec gcc6-armv7 pip install -U conan_package_tools && \
+sudo docker exec gcc6-armv7 pip install -U conan && \
 sudo docker exec gcc6-armv7 conan user && \
 sudo docker exec gcc6-armv7 conan install gtest/1.8.0@bincrafters/stable -s arch=armv7 -s compiler=gcc -s compiler.version=6 -s compiler.libcxx=libstdc++ --build && \
 sudo docker stop gcc6-armv7 && \
