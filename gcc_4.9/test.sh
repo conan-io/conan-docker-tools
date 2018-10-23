4c0f61b58691a0891e32201f0657d77ae6ac2071
Uilian Ries <uilianries@gmail.com>
2018-10-23 16:49:51 -0300

    #53 Install python on GCC 4.9
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- gcc_4.9/test.sh
+++ gcc_4.9/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo docker run -t -d --name gcc49 conanio/gcc49 && \
-sudo docker exec gcc49 sudo pip install -U conan_package_tools && \
-sudo docker exec gcc49 sudo pip install -U conan && \
+sudo docker exec gcc49 pip install -U conan_package_tools && \
+sudo docker exec gcc49 pip install -U conan && \
 sudo docker exec gcc49 conan user && \
 sudo docker exec gcc49 conan install gtest/1.8.0@bincrafters/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=4.9 -s compiler.libcxx=libstdc++ --build && \
 sudo docker exec gcc49 conan install gtest/1.8.0@bincrafters/stable -s arch=x86 -s compiler=gcc -s compiler.version=4.9 -s compiler.libcxx=libstdc++ --build && \
