40e299b39f8384cda098ab503f3490289f6d656b
Uilian Ries <uilianries@gmail.com>
2018-10-23 15:47:27 -0300

    #53 Intall python on GCC 7 ARMv7hf
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- gcc_7-armv7hf/test.sh
+++ gcc_7-armv7hf/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo docker run -t -d --name gcc7-armv7hf conanio/gcc6-armv7hf && \
-sudo docker exec gcc7-armv7hf sudo pip install -U conan_package_tools && \
-sudo docker exec gcc7-armv7hf sudo pip install -U conan && \
+sudo docker exec gcc7-armv7hf pip install -U conan_package_tools && \
+sudo docker exec gcc7-armv7hf pip install -U conan && \
 sudo docker exec gcc7-armv7hf conan user && \
 sudo docker exec gcc7-armv7hf conan install gtest/1.8.0@bincrafters/stable -s arch=armv7hf -s compiler=gcc -s compiler.version=7 -s compiler.libcxx=libstdc++ --build && \
 sudo docker stop gcc7-armv7hf && \
