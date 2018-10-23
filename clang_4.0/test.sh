ea8a2b4d4ad724bd2d8b8f169a21c87b7c743f88
Uilian Ries <uilianries@gmail.com>
2018-10-23 17:36:37 -0300

    #53 Install python on Clang 4
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- clang_4.0/test.sh
+++ clang_4.0/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo docker run -t -d --name clang40 conanio/clang40 && \
-sudo docker exec clang40 sudo pip install -U conan_package_tools && \
-sudo docker exec clang40 sudo pip install -U conan && \
+sudo docker exec clang40 pip install -U conan_package_tools && \
+sudo docker exec clang40 pip install -U conan && \
 sudo docker exec clang40 conan user && \
 sudo docker exec clang40 conan install gtest/1.8.0@bincrafters/stable -s arch=x86_64 -s compiler=clang -s compiler.version=4.0 -s compiler.libcxx=libstdc++ --build && \
 sudo docker exec clang40 conan install gtest/1.8.0@bincrafters/stable -s arch=x86 -s compiler=clang -s compiler.version=4.0 -s compiler.libcxx=libstdc++ --build && \
