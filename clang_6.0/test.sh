fd063ab4ccf413d92c3962788a8c33c30a9b2773
Uilian Ries <uilianries@gmail.com>
2018-10-23 17:01:47 -0300

    #53 Install python on Clang 6
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- clang_6.0/test.sh
+++ clang_6.0/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo docker run -t -d --name clang60 conanio/clang60 && \
-sudo docker exec clang60 sudo pip install -U conan_package_tools && \
-sudo docker exec clang60 sudo pip install -U conan && \
+sudo docker exec clang60 pip install -U conan_package_tools && \
+sudo docker exec clang60 pip install -U conan && \
 sudo docker exec clang60 conan user && \
 sudo docker exec clang60 conan install gtest/1.8.0@bincrafters/stable -s arch=x86_64 -s compiler=clang -s compiler.version=6.0 -s compiler.libcxx=libstdc++ --build && \
 sudo docker exec clang60 conan install gtest/1.8.0@bincrafters/stable -s arch=x86 -s compiler=clang -s compiler.version=6.0 -s compiler.libcxx=libstdc++ --build && \
