1193c5e078c7b170ebaeecbc6b9b54c899a1f213
Uilian Ries <uilianries@gmail.com>
2018-10-23 16:57:21 -0300

    #53 Install python on Clang 7 x86
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- clang_7-x86/test.sh
+++ clang_7-x86/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo docker run -t -d --name clang7-x86 conanio/clang7-x86 && \
-sudo docker exec clang7-x86 sudo pip install -U conan_package_tools && \
-sudo docker exec clang7-x86 sudo pip install -U conan && \
+sudo docker exec clang7-x86 pip install -U conan_package_tools && \
+sudo docker exec clang7-x86 pip install -U conan && \
 sudo docker exec clang7-x86 conan user && \
 sudo docker exec clang7-x86 conan install gtest/1.8.0@bincrafters/stable -s arch=x86 -s compiler=clang -s compiler.version=7.0 -s compiler.libcxx=libstdc++ --build && \
 sudo docker exec clang7-x86 conan install gtest/1.8.0@bincrafters/stable -s arch=x86 -s compiler=clang -s compiler.version=7.0 -s compiler.libcxx=libc++ --build && \
