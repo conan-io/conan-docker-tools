31419fa74d12aa9deb951ef273a9dd6b745581bc
Uilian Ries <uilianries@gmail.com>
2018-10-29 16:51:13 -0300

    #66 Run tests with sudo
    - Update python path with update-alternatives
    - Add multiple tests using sudo command
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -37,7 +37,6 @@ class ConanDockerTools(object):
         docker_username = os.getenv("DOCKER_USERNAME", "conanio")
         docker_login_username = os.getenv("DOCKER_LOGIN_USERNAME", "lasote")
         docker_build_tag = os.getenv("DOCKER_BUILD_TAG", "latest")
-        sudo_command = self._get_boolean_var("DOCKER_SUDO")
         docker_archs = os.getenv("DOCKER_ARCHS").split(",") if os.getenv("DOCKER_ARCHS") else ["x86_64"]
         os.environ["DOCKER_USERNAME"] = docker_username
         os.environ["DOCKER_BUILD_TAG"] = docker_build_tag
@@ -49,15 +48,9 @@ class ConanDockerTools(object):
                                                         "docker_username, docker_login_username, "
                                                         "gcc_versions, "
                                                         "clang_versions, build_server, docker_build_tag, "
-                                                        "docker_archs, sudo_command")
+                                                        "docker_archs")
         return Variables(docker_upload, docker_password, docker_username, docker_login_username,
-                         gcc_versions, clang_versions, build_server, docker_build_tag, docker_archs
-                         , sudo_command)
-
-    def _get_sudo_command(self):
-        """ Return sudo command name when required
-        """
-        return "sudo" if self.variables.sudo_command else ""
+                         gcc_versions, clang_versions, build_server, docker_build_tag, docker_archs)
 
     def _get_boolean_var(self, var, default="false"):
         """ Parse environment variable as boolean type
@@ -81,13 +74,12 @@ class ConanDockerTools(object):
         subprocess.call('docker run --rm -i lukasmartinelli/hadolint < %s/Dockerfile' % build_dir,
                         shell=True)
 
-    def test(self, arch, compiler_name, compiler_version, service, sudo_command):
+    def test(self, arch, compiler_name, compiler_version, service):
         """Validate Docker image by Conan install
         :param arch: Name of he architecture
         :param compiler_name: Compiler to be specified as conan setting e.g. clang
         :param compiler_version: Compiler version to be specified as conan setting e.g. 3.8
         :param service: Docker compose service name
-        :param sudo_command: Linux sudo command
         """
         logging.info("Testing Docker by service %s." % service)
         try:
@@ -95,42 +87,48 @@ class ConanDockerTools(object):
             libcxx_list = ["libstdc++"] if compiler_name == "gcc" else ["libstdc++", "libc++"]
             subprocess.check_call("docker run -t -d --name %s %s" % (service, image), shell=True)
 
-            output = subprocess.check_output("docker exec %s python3 --version" % service, shell=True)
-            assert "Python 3" in output.decode()
-            logging.info("Found %s" % output.decode().rstrip())
+            for sudo_command in ["", "sudo", "sudo -E"]:
+                logging.info("Testing command prefix: '{}'".format(sudo_command))
+                output = subprocess.check_output("docker exec %s %s python3 --version" % (service, sudo_command), shell=True)
+                assert "Python 3" in output.decode()
+                logging.info("Found %s" % output.decode().rstrip())
 
-            output = subprocess.check_output("docker exec %s pip --version" % service, shell=True)
-            assert "python 3" in output.decode()
-            logging.info("Found pip (Python 3)")
+                output = subprocess.check_output("docker exec %s %s pip --version" % (service, sudo_command), shell=True)
+                assert "python 3" in output.decode()
+                logging.info("Found pip (Python 3)")
 
-            output = subprocess.check_output("docker exec %s pip show conan" % service, shell=True)
-            assert "python3" in output.decode()
-            logging.info("Found Conan (Python 3)")
+                output = subprocess.check_output("docker exec %s %s pip3 --version" % (service, sudo_command), shell=True)
+                assert "python 3" in output.decode()
+                logging.info("Found pip3 (Python 3)")
 
-            output = subprocess.check_output("docker exec %s python --version" % service, shell=True)
-            assert "Python 3" in output.decode()
-            logging.info("Default Python version: %s" % output.decode().rstrip())
+                output = subprocess.check_output("docker exec %s %s pip show conan" % (service, sudo_command), shell=True)
+                assert "python3" in output.decode()
+                logging.info("Found Conan (Python 3)")
 
-            subprocess.check_call("docker exec %s %s pip install --no-cache-dir -U conan_package_tools" %
-                                  (service, sudo_command), shell=True)
-            subprocess.check_call("docker exec %s %s pip install --no-cache-dir -U conan" %
-                                 (service, sudo_command), shell=True)
-            subprocess.check_call("docker exec %s conan user" % service, shell=True)
+                output = subprocess.check_output("docker exec %s %s python --version" % (service, sudo_command), shell=True)
+                assert "Python 3" in output.decode()
+                logging.info("Default Python version: %s" % output.decode().rstrip())
 
-            if compiler_name == "clang" and compiler_version == "7":
-                    compiler_version = "7.0" # FIXME: Remove this when fixed in conan
+                subprocess.check_call("docker exec %s %s pip install --no-cache-dir -U conan_package_tools" %
+                                    (service, sudo_command), shell=True)
+                subprocess.check_call("docker exec %s %s pip install --no-cache-dir -U conan" %
+                                    (service, sudo_command), shell=True)
+                subprocess.check_call("docker exec %s %s conan user" % (service, sudo_command), shell=True)
 
-            subprocess.check_call("docker exec %s conan install lz4/1.8.3@bincrafters/stable -s "
-                                  "arch=%s -s compiler=%s -s compiler.version=%s --build" %
-                                  (service, arch, compiler_name,
-                                   compiler_version), shell=True)
+                if compiler_name == "clang" and compiler_version == "7":
+                        compiler_version = "7.0" # FIXME: Remove this when fixed in conan
 
-            for libcxx in libcxx_list:
-                subprocess.check_call("docker exec %s conan install gtest/1.8.1@bincrafters/stable -s "
-                                      "arch=%s -s compiler=%s -s compiler.version=%s "
-                                      "-s compiler.libcxx=%s --build" %
-                                      (service, arch, compiler_name,
-                                       compiler_version, libcxx), shell=True)
+                subprocess.check_call("docker exec %s %s conan install lz4/1.8.3@bincrafters/stable -s "
+                                    "arch=%s -s compiler=%s -s compiler.version=%s --build" %
+                                    (service, sudo_command, arch, compiler_name,
+                                    compiler_version), shell=True)
+
+                for libcxx in libcxx_list:
+                    subprocess.check_call("docker exec %s %s conan install gtest/1.8.1@bincrafters/stable -s "
+                                        "arch=%s -s compiler=%s -s compiler.version=%s "
+                                        "-s compiler.libcxx=%s --build" %
+                                        (service, sudo_command, arch, compiler_name,
+                                        compiler_version, libcxx), shell=True)
         finally:
             subprocess.call("docker stop %s" % service, shell=True)
             subprocess.call("docker rm %s" % service, shell=True)
@@ -167,11 +165,10 @@ class ConanDockerTools(object):
                     tag_arch = "" if arch == "x86_64" else "-%s" % arch
                     service = "%s%s%s" % (compiler.name, version.replace(".", ""), tag_arch)
                     build_dir = "%s_%s%s" % (compiler.name, version, tag_arch)
-                    sudo_command =  self._get_sudo_command()
 
-                    self.linter(build_dir)
+                    #self.linter(build_dir)
                     self.build(service)
-                    self.test(arch, compiler.name, version, service, sudo_command)
+                    self.test(arch, compiler.name, version, service)
                     self.deploy(service)
 
         if self.variables.build_server:
