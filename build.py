92b2b0b9387aef3cfa4ead9966d9a6566fb64d6f
Uilian Ries <uilianries@gmail.com>
2019-05-13 20:10:36 -0300

    Remove cross from distro
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -382,8 +382,8 @@ class ConanDockerTools(object):
                 for version in compiler.versions:
                     for distro in self.variables.docker_distro:
                         distro = "-%s" % distro
-                        service = "%s%s%s%s%s" % (cross, compiler.name, version.replace(".", ""), distro)
-                        build_dir = "%s%s_%s%s" % (cross, compiler.name, version, distro)
+                        service = "%s%s%s%s" % (compiler.name, version.replace(".", ""), distro)
+                        build_dir = "%s_%s%s" % (compiler.name, version, distro)
 
                         self.service = service
                         self.login()
