2bb2d95bc3374b7ef8d804768f6af32778f715ab
Uilian Ries <uilianries@gmail.com>
2019-07-03 18:56:26 -0300

    Remove Conan user
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- android-clang_8/run.sh
+++ android-clang_8/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo docker run --rm -v ~/.conan/data:/home/conan/.conan/data -it conanio/android-clang8 /bin/bash
+sudo docker run --rm -v ~/.conan/data:/root/.conan/data -it conanio/android-clang8 /bin/bash
