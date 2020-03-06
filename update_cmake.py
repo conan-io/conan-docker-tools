1c0eef080844ee6ccf337055fdbb3f4135564c01
Croydon <git@cr0ydon.com>
2020-03-06 12:03:45 +0100

    Install CMake via pip

--- update_cmake.py
+++ update_cmake.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
 import os
 
 if __name__ == "__main__":
 
-    for old, new in [("3.16.1", "3.16.4"), ("v3.16", "v3.16")]:
+    old = "cmake==3.15.*"
+    new = "cmake==3.16.*"
 
-        for root, _, filenames in os.walk("./"):
-            for filename in filenames:
-                if filename == "Dockerfile":
-                    path = os.path.join(root, filename)
-                    with open(path) as file:
-                        data = file.read()
+    for root, _, filenames in os.walk("./"):
+        for filename in filenames:
+            if filename == "Dockerfile":
+                path = os.path.join(root, filename)
+                with open(path) as file:
+                    data = file.read()
 
-                    data = data.replace(old, new)
+                data = data.replace(old, new)
 
-                    with open(path, "w") as file:
-                        file.write(data)
+                with open(path, "w") as file:
+                    file.write(data)
