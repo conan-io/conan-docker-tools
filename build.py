809c5cf0f7e4b05d9403fff789c1bfe587d47c03
Croydon <git@cr0ydon.com>
2020-04-18 09:07:41 +0200

    build.py: Downgrade gtest to fix GCC 4.6 + zlib from CCI

--- build.py
+++ build.py
@@ -204,7 +204,7 @@ class ConanDockerTools(object):
                             (self.service, arch, compiler_name,
                             compiler_version), shell=True)
 
-        subprocess.check_call('docker exec %s conan install gtest/1.10.0@ -s '
+        subprocess.check_call('docker exec %s conan install gtest/1.8.1@ -s '
                             'arch=%s -s compiler="%s" -s compiler.version=%s '
                             '-s compiler.runtime=MD --build' %
                             (self.service, arch, compiler_name,
@@ -274,7 +274,7 @@ class ConanDockerTools(object):
 
         for libcxx in libcxx_list:
             subprocess.check_call(
-                "docker exec %s conan install gtest/1.10.0@ -s "
+                "docker exec %s conan install gtest/1.8.1@ -s "
                 "arch=%s -s compiler=%s -s compiler.version=%s "
                 "-s compiler.libcxx=%s --build" % (self.service, arch, compiler_name,
                                                 compiler_version, libcxx),
@@ -318,8 +318,8 @@ class ConanDockerTools(object):
             subprocess.check_call("docker run -t -d --name %s %s" % (self.service,
                 self.created_image_name), shell=True)
             output = subprocess.check_output(
-                "docker exec %s conan install -r conan-center zlib/1.2.11@conan/stable" % (self.service), shell=True)
-            assert "zlib/1.2.11@conan/stable: Package installed" in output.decode()
+                "docker exec %s conan install -r conan-center zlib/1.2.11@" % (self.service), shell=True)
+            assert "zlib/1.2.11: Package installed" in output.decode()
         finally:
             subprocess.call("docker stop %s" % self.service, shell=True)
             subprocess.call("docker rm %s" % self.service, shell=True)
