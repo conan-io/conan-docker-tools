1fcdb2702861e51bd2b5f0d437934974f4d76915
Uilian Ries <uilianries@gmail.com>
2020-05-22 16:58:41 -0300

    Build CCI images on CI
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -50,6 +50,7 @@ class ConanDockerTools(object):
         build_server = self._get_boolean_var("BUILD_CONAN_SERVER_IMAGE")
         build_tests = self._get_boolean_var("BUILD_CONAN_TESTS")
         build_test_azure = self._get_boolean_var("BUILD_CONAN_TEST_AZURE")
+        build_cci = self._get_boolean_var("BUILD_CONAN_CCI")
         docker_password = os.getenv("DOCKER_PASSWORD", "").replace('"', '\\"')
         docker_username = os.getenv("DOCKER_USERNAME", "conanio")
         docker_login_username = os.getenv("DOCKER_LOGIN_USERNAME", "lasote")
@@ -77,11 +78,12 @@ class ConanDockerTools(object):
             "clang_versions, visual_versions, build_server, "
             "docker_build_tag, docker_archs, sudo_command, "
             "docker_upload_only_when_stable, docker_cross, docker_cache, "
-            "build_tests, build_test_azure docker_upload_retry")
+            "build_tests, build_test_azure docker_upload_retry, build_cci")
         return Variables(docker_upload, docker_password, docker_username, docker_login_username,
                          gcc_versions, docker_distro, clang_versions, visual_versions, build_server,
                          docker_build_tag, docker_archs, sudo_command, docker_upload_only_when_stable,
-                         docker_cross, docker_cache, build_tests, build_test_azure, docker_upload_retry)
+                         docker_cross, docker_cache, build_tests, build_test_azure, docker_upload_retry,
+                         build_cci)
 
     def _get_boolean_var(self, var, default="false"):
         """ Parse environment variable as boolean type
@@ -420,6 +422,9 @@ class ConanDockerTools(object):
                     tag_arch = "" if arch == "x86_64" else "-%s" % arch
                     service = "%s%s%s%s" % (cross, compiler.name, version.replace(".", ""), tag_arch)
                     build_dir = "%s%s_%s%s" % (cross, compiler.name, version, tag_arch)
+                    if self.variables.build_cci:
+                        service = "%s%s-cci" % (compiler.name, version.replace(".", ""))
+                        build_dir = "%s-cci" % (compiler.name)
 
                     self.service = service
                     self.login()
