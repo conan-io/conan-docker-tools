7a3e8a49b463e14cb7483e41e349905e4315bdcf
Croydon <git@cr0ydon.com>
2020-08-27 20:29:22 +0200

    replace all CMake versions with vars and fix help folder removal

--- update_cmake.py
+++ update_cmake.py
@@ -4,10 +4,8 @@ import os
 if __name__ == "__main__":
 
     for old, new in [
-        ("3.17.1", "3.18.2"),
-        ("v3.17", "v3.18"),
         ("CMAKE_VERSION_MAJOR_MINOR=3.17", "CMAKE_VERSION_MAJOR_MINOR=3.18"),
-        ("CMAKE_VERSION_FULL=3.17.1", "CMAKE_VERSION_MAJOR_MINOR=3.18.2"),
+        ("CMAKE_VERSION_FULL=3.17.1", "CMAKE_VERSION_FULL=3.18.2"),
         ]:
 
         for root, _, filenames in os.walk("./"):
