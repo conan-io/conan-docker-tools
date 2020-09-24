5f386ab0a394b46663ff0defaf716613b8380daa
Uilian Ries <uilianries@gmail.com>
2020-09-24 11:19:57 -0300

    Remove Agent Docker image
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -51,7 +51,6 @@ class ConanDockerTools(object):
         build_tests = self._get_boolean_var("BUILD_CONAN_TESTS")
         build_test_azure = self._get_boolean_var("BUILD_CONAN_TEST_AZURE")
         build_test_agent = self._get_boolean_var("BUILD_CONAN_TEST_AGENT")
-        build_test_docker = self._get_boolean_var("BUILD_CONAN_TEST_DOCKER")
         docker_password = os.getenv("DOCKER_PASSWORD", "").replace('"', '\\"')
         docker_username = os.getenv("DOCKER_USERNAME", "conanio")
         docker_login_username = os.getenv("DOCKER_LOGIN_USERNAME", "lasote")
@@ -80,12 +79,12 @@ class ConanDockerTools(object):
             "docker_build_tag, docker_archs, sudo_command, "
             "docker_upload_only_when_stable, docker_cross, docker_cache, "
             "build_tests, build_test_azure docker_upload_retry, "
-            "build_test_agent, build_test_docker")
+            "build_test_agent")
         return Variables(docker_upload, docker_password, docker_username, docker_login_username,
                          gcc_versions, docker_distro, clang_versions, visual_versions, build_server,
                          docker_build_tag, docker_archs, sudo_command, docker_upload_only_when_stable,
                          docker_cross, docker_cache, build_tests, build_test_azure,
-                         docker_upload_retry, build_test_agent, build_test_docker)
+                         docker_upload_retry, build_test_agent)
 
     def _get_boolean_var(self, var, default="false"):
         """ Parse environment variable as boolean type
@@ -402,7 +401,6 @@ class ConanDockerTools(object):
         for image_name, build_image, build_dir in [
                 ("conantests", self.variables.build_tests, "conan_tests"),
                 ("conantestazure", self.variables.build_test_azure, "conan_test_azure"),
-                ("conantestdocker", self.variables.build_test_docker, "conan_test_docker"),
                 ("conantestagent", self.variables.build_test_agent, "jenkins-jnlp-slave"),
             ]:
             if build_image:
