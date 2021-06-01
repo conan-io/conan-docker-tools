77fb4822faab597632dae6dd5fb58d178ec1d40a
Uilian Ries <uilianries@gmail.com>
2021-06-01 14:44:24 -0300

    Fix container name
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test/standard/run.sh
+++ test/standard/run.sh
@@ -8,7 +8,7 @@ container="$compiler-"$(openssl rand -hex 2)
 docker run -t -d -v ${PWD}:/tmp/project --name ${container} ${DOCKER_USERNAME}/${compiler}-ubuntu16.04
 docker exec ${container} git clone https://github.com/conan-io/conan-center-index.git
 docker exec ${container} /tmp/project/test/standard/build_imagl.sh
-docker exec ${compcontaineriler} /tmp/project/test/standard/build_libsolace.sh
+docker exec ${container} /tmp/project/test/standard/build_libsolace.sh
 
 docker stop ${container}
 docker rm -f ${container}
