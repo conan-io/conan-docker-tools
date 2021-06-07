f0483ef37e3483da429b5df370220d35ef019bb3
Uilian Ries <uilianries@gmail.com>
2021-06-07 12:20:50 -0300

    Drop Clang 9
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test/simple/main.c
+++ test/simple/main.c
@@ -2,13 +2,26 @@
 #include <time.h>
 #include <stdlib.h>
 
-int main(void) {
+#include <pthread.h>
+
+void* show_date(void* data) {
     time_t rawtime;
     struct tm * timeinfo;
-
     time(&rawtime);
     timeinfo = localtime(&rawtime);
     printf("Current local time and date: %s", asctime(timeinfo));
+    return 0;
+}
 
+int main(void) {
+    int result;
+    pthread_t worker;
+
+    result = pthread_create(&worker, NULL, show_date, NULL);
+    if (result) {
+        perror("Could not create thread");
+        return EXIT_FAILURE;
+    }
+    pthread_join(worker, NULL);
     return EXIT_SUCCESS;
 }
--- test/simple/main.cpp
+++ test/simple/main.cpp
@@ -3,10 +3,17 @@
 
 #include <iostream>
 #include <chrono>
+#include <thread>
 
-int main(void) {
+
+void show_date(void) {
     const auto now = std::chrono::system_clock::now();
     const std::time_t now_time = std::chrono::system_clock::to_time_t(now);
     std::cout << "Current date: " << std::ctime(&now_time) << std::endl;
+}
+
+int main(void) {
+    std::thread worker(show_date);
+    worker.join();
     return EXIT_SUCCESS;
 }
