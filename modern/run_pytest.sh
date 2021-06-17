26a474e85272ce62f71d681f851f3680c2b0f156
jgsogo <jgsogo@gmail.com>
2021-06-17 10:33:17 +0200

    we are already in the folder

--- modern/run_pytest.sh
+++ modern/run_pytest.sh
@@ -9,9 +9,9 @@ echo CLANG_VERSION $CLANG_VERSION
 if [ -z ${GCC_VERSION+x} ]; then
   docker_image="docker.io/$DOCKER_USERNAME/clang$CLANG_VERSION-ubuntu16.04:$CONAN_VERSION"
   echo "docker image: $docker_image"
-  pytest modern/tests --image $docker_image
+  pytest tests --image $docker_image
 else
   docker_image="docker.io/$DOCKER_USERNAME/gcc$GCC_VERSION-ubuntu16.04:$CONAN_VERSION"
   echo "docker image: $docker_image"
-  pytest modern/tests --image $docker_image
+  pytest tests --image $docker_image
 fi
