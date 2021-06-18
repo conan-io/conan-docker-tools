9ccc531ffeda561ee701b50ef87af779548bccbb
jgsogo <jgsogo@gmail.com>
2021-06-18 12:00:52 +0200

    testing tools are installed

--- modern/tests/test_installed/test_gcc.py
+++ modern/tests/test_installed/test_gcc.py
@@ -11,12 +11,7 @@ class TestGccCompiler:
         first_line = out.splitlines()[0]
         assert first_line == f'gcc (GCC) {expected.compiler.version}'
 
-
-@pytest.mark.compiler('gcc')
-@pytest.mark.service('builder', 'deploy', 'jenkins')
-class TestGccGFortranCompiler:
-
-    def test_version(self, container, expected):
+    def test_gfortran_version(self, container, expected):
         out, _ = container.exec(['gfortran', '--version'])
         assert expected.compiler.name == 'gcc'
         first_line = out.splitlines()[0]
