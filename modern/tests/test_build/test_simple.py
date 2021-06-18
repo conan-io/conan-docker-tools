27fb8e9cc83b0af16bae461838195320d25ca8a7
jgsogo <jgsogo@gmail.com>
2021-06-18 13:40:56 +0200

    run ldd, but no testing so far

--- modern/tests/test_build/test_simple.py
+++ modern/tests/test_build/test_simple.py
@@ -7,8 +7,9 @@ from fixtures.container import run_container
 # the binary generated can run in a vanilla image
 
 
+@pytest.mark.compiler('gcc')
 @pytest.mark.service('deploy', 'jenkins')
-def test_simple(container, expected):
+def test_gcc_simple(container, expected):
     build_directory = '/tmp/build/simple'
     # Compile the project
     with container.working_dir(build_directory):
@@ -22,6 +23,52 @@ def test_simple(container, expected):
         assert 'Current date' in out, f"out: '{out}' err: '{err}'"
 
         # Check linked libs
+        out, err = container.exec(['ldd', 'example-c'])
+        # TODO: Test linked libraries here (gcc)
+        print(out)
+        print(err)
+
+        out, err = container.exec(['ldd', 'example-cpp'])
+        # TODO: Test linked libraries here (gcc)
+        print(out)
+        print(err)
+
+    # Check we can run these executables in vanilla image
+    vanilla_img = f"{expected.distro.name}:{expected.distro.version}"
+    with run_container(vanilla_img, tmpdirname=container._tmpfolder) as vanilla:
+        with vanilla.working_dir(build_directory):
+            out, err = vanilla.exec(['./example-c'])
+            assert 'Current local time and date' in out, f"out: '{out}' err: '{err}'"
+
+            out, err = vanilla.exec(['./example-cpp'])
+            assert 'Current date' in out, f"out: '{out}' err: '{err}'"
+
+
+@pytest.mark.compiler('clang')
+@pytest.mark.service('deploy', 'jenkins')
+def test_clang_simple(container, expected):
+    build_directory = '/tmp/build/simple'
+    # Compile the project
+    with container.working_dir(build_directory):
+        container.exec(['cmake', '/tmp/workingdir/simple', '-DCMAKE_BUILD_TYPE=Release'])
+        container.exec(['cmake', '--build', '.'])
+
+        # Check it runs
+        out, err = container.exec(['./example-c'])
+        assert 'Current local time and date' in out, f"out: '{out}' err: '{err}'"
+        out, err = container.exec(['./example-cpp'])
+        assert 'Current date' in out, f"out: '{out}' err: '{err}'"
+
+        # Check linked libs
+        out, err = container.exec(['ldd', 'example-c'])
+        # TODO: Test linked libraries here (clang)
+        print(out)
+        print(err)
+
+        out, err = container.exec(['ldd', 'example-cpp'])
+        # TODO: Test linked libraries here (clang)
+        print(out)
+        print(err)
 
     # Check we can run these executables in vanilla image
     vanilla_img = f"{expected.distro.name}:{expected.distro.version}"
