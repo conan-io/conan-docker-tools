e2528155474796ab37d03f4d0e759edde732680c
Uilian Ries <uilianries@gmail.com>
2021-07-16 05:05:33 -0300

    Validate installed and not installed tools for new Docker images (#292)
    * #287 Validate installed tools
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #287 Skip double-quote
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #287 Skip double-quote
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #287 Skip double-quote
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #287 Java is optional due Jenkins
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>
    
    * #287 Expect version according distro
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- modern/tests/test_installed/test_installed.py
+++ modern/tests/test_installed/test_installed.py
@@ -1,9 +1,35 @@
+import pytest
+
+
+expected_versions = {
+    "pkg-config": {"16.04": "0.29.1"},
+    "make": {"16.04": "4.1"},
+    "autoconf": {"16.04": "2.69"},
+    "autoreconf": {"16.04": "2.69"},
+    "perl": {"16.04": "5.22.1"},
+    "wget": {"16.04": "1.17.1"},
+    "curl": {"16.04": "7.47.0"},
+    "git": {"16.04": "2.7.4"},
+    "svn": {"16.04": "1.9.3"},
+    "xz": {"16.04": "5.1.0"},
+    "nasm": {"16.04": "2.11.08"},
+}
+
+
 def test_cmake_version(container, expected):
-    output, _ = container.exec(['cmake', '--version'])
+    output, _ = container.exec(["cmake", "--version"])
     first_line = output.splitlines()[0]
-    assert first_line.strip() == f'cmake version {expected.cmake}'
+    assert first_line.strip() == f"cmake version {expected.cmake}"
 
 
 def test_python_version(container, expected):
-    output, _ = container.exec(['python', '--version'])
-    assert output.strip() == f'Python {expected.python}'
+    output, _ = container.exec(["python", "--version"])
+    assert output.strip() == f"Python {expected.python}"
+
+
+@pytest.mark.parametrize("tool", expected_versions.keys())
+def test_installed_system_package_version(container, expected, tool):
+    output, _ = container.exec([tool, "--version"])
+    if output == "":
+        output, _ = container.exec([tool, "-v"])
+    assert expected_versions[tool][expected.distro.version.full_version] in output
---
+++ modern/tests/test_installed/test_not_installed.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+@pytest.mark.parametrize(
+    "tool",
+    [
+        "vim",
+        "vi",
+        "nano",
+        "meson",
+        "ninja",
+        "bazel",
+        "scons",
+        "qmake",
+        "xmake",
+        "premake5",
+        "b2",
+        "lua",
+    ],
+)
+def test_not_installed(container, tool):
+    output, _ = container.exec([tool, "--version"])
+    first_line = output.splitlines()[0]
+    assert f'exec: \\"{tool}\\": executable file not found in $PATH' in first_line.strip()
