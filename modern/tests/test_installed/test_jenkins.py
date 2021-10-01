b693dc3507cf16e7ec5db50e002765420b747651
jgsogo <jgsogo@gmail.com>
2021-10-01 16:35:46 +0200

    those are error messages

--- modern/tests/test_installed/test_jenkins.py
+++ modern/tests/test_installed/test_jenkins.py
@@ -6,11 +6,10 @@ class TestJenkinsAgent:
 
     def test_java(self, container):
         out, err = container.exec(['java', '-version'])
-        first_line = out.splitlines()[0]
         # Check Java, up to minor version
-        assert 'openjdk version "1.8' in first_line.strip(), f"out: '{out}' err: '{err}'"
+        assert 'openjdk version "1.8' in err, f"out: '{out}' err: '{err}'"
 
     def test_entrypoint(self, container):
         out, err = container.exec(['/opt/entrypoint.sh'])
         # It shows help (missing arguments), but we know it works
-        assert 'java -jar agent.jar [options...] <secret key> <agent name>' in out, f"out: '{out}' err: '{err}'"
+        assert 'java -jar agent.jar [options...] <secret key> <agent name>' in err, f"out: '{out}' err: '{err}'"
