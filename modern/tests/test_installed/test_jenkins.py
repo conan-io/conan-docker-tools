1ed562c7ff5a674f30638a793b9ca817abac2c82
jgsogo <jgsogo@gmail.com>
2021-10-01 16:28:55 +0200

    show out and err

--- modern/tests/test_installed/test_jenkins.py
+++ modern/tests/test_installed/test_jenkins.py
@@ -5,12 +5,12 @@ import pytest
 class TestJenkinsAgent:
 
     def test_java(self, container):
-        out, _ = container.exec(['java', '-version'])
+        out, err = container.exec(['java', '-version'])
         first_line = out.splitlines()[0]
         # Check Java, up to minor version
-        assert 'openjdk version "1.8' in first_line.strip()
+        assert 'openjdk version "1.8' in first_line.strip(), f"out: '{out}' err: '{err}'"
 
-    def test_entrypoint(self):
-        out, _ = container.exec(['/opt/entrypoint.sh'])
+    def test_entrypoint(self, container):
+        out, err = container.exec(['/opt/entrypoint.sh'])
         # It shows help (missing arguments), but we know it works
-        assert 'java -jar agent.jar [options...] <secret key> <agent name>' in out
+        assert 'java -jar agent.jar [options...] <secret key> <agent name>' in out, f"out: '{out}' err: '{err}'"
