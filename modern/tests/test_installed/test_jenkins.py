7db08c1e28b968a4b0fbcf57424aca843b1d48f9
Uilian Ries <uilianries@gmail.com>
2023-06-15 19:30:55 +0200

    [hotfix] Update certificates paths after installing java (#527)

--- modern/tests/test_installed/test_jenkins.py
+++ modern/tests/test_installed/test_jenkins.py
@@ -13,3 +13,7 @@ class TestJenkinsAgent:
         out, err = container.exec(['/opt/entrypoint.sh'])
         # It shows help (missing arguments), but we know it works
         assert 'java -jar agent.jar [options...] <secret key> <agent name>' in err, f"out: '{out}' err: '{err}'"
+
+    def test_cacert(self, container):
+        out, err = container.exec(['keytool', '-list', '-keystore', '/etc/ssl/certs/java/cacerts', '-storepass', 'changeit', '-storetype', 'JKS'])
+        assert 'Keystore type: JKS' in out, f"out: '{out}' err: '{err}'"
