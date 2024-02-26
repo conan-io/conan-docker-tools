44bd4f27727cc8efc325c376d17457d7e74a1c28
Uilian Ries <uilianries@gmail.com>
2024-02-26 16:33:29 +0100

    Validate agent version
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- modern/tests/test_installed/test_jenkins.py
+++ modern/tests/test_installed/test_jenkins.py
@@ -5,14 +5,22 @@ import pytest
 class TestJenkinsAgent:
 
     def test_java(self, container):
+        # TODO: Pass expected JDK version as parameter
         out, err = container.exec(['java', '-version'])
         # Check Java, up to minor version
         assert 'openjdk version' in err, f"out: '{out}' err: '{err}'"
 
+    def test_agent(self, container):
+        """The Jenkins agent version should match the image tag version
+        """
+        out, err = container.exec(['java', '-jar', '/usr/share/jenkins/agent.jar', '-version'])
+        image = container.image
+        assert out.strip() in image, f"out: '{out}' err: '{err}'"
+
     def test_entrypoint(self, container):
-        out, err = container.exec(['/opt/entrypoint.sh'])
+        out, err = container.exec(['/opt/entrypoint.sh', '-help'])
         # It shows help (missing arguments), but we know it works
-        assert 'java -jar agent.jar [options...] <secret key> <agent name>' in err, f"out: '{out}' err: '{err}'"
+        assert 'Show this help message' in out, f"out: '{out}' err: '{err}'"
 
     def test_cacert(self, container):
         out, err = container.exec(['keytool', '-list', '-keystore', '/etc/ssl/certs/java/cacerts', '-storepass', 'changeit', '-storetype', 'JKS'])
